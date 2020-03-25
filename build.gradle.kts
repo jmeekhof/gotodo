98b3e965ff5fa07b1146f096741414ae302f9260
Josh Meekhof <jmeekhof@twotheleft.com>
2020-03-24 20:05:18 -0400

    Setup ENV vars to pass to docker compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,27 @@
 plugins {
   id("com.avast.gradle.docker-compose") version "0.10.9"
+  id("net.saliman.properties") version "1.5.1"
+}
+
+val mongoHost: String by project
+val mongoUser: String by project
+val mongoPassword: String by project
+val mongoDatabase: String by project
+val mongoPort: String by project
+val npmRegistry: String? by project
+
+val envMap = hashMapOf<String, String>(
+        "MONGO_HOST" to mongoHost,
+        "MONGO_USER" to mongoUser,
+        "MONGO_PASSWORD" to mongoPassword,
+        "MONGO_DATABASE" to mongoDatabase
+)
+
+npmRegistry?.let {
+    envMap["npm_config_registry"] = it
 }
 
 dockerCompose {
     removeVolumes = false
+    environment.putAll(envMap)
 }
